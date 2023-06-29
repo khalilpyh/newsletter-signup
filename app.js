03f602a83a5d62b18139cb01013c4a4052bd8462
khalilpyh <khalilpyh@gmail.com>
2023-06-29 02:52:58 -0400

    add user subscription, page transfer

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 //import modules
 const express = require("express");
 const bodyParser = require("body-parser");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+//listing ID: c282788d7c
+//API key: 7f8011d4f89b8f8978d064ed799b58a4-us13
 
 //initialize express
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true })); //use body-parser
 app.use(express.static("public")); //use local css and images
+
+//set up Mailchimp
+mailchimp.setConfig({
+  apiKey: "7f8011d4f89b8f8978d064ed799b58a4-us13",
+  server: "us13",
+});
 
 //Landing page -  Get
 app.get("/", function (req, res) {
@@ -18,11 +28,44 @@ app.post("/", function (req, res) {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
-  console.log(firstName, lastName, email);
+  const listID = "c282788d7c";
+
+  //create a new user base on given infomation
+  const newUser = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  //add user to the email list
+  const run = async () => {
+    const response = await mailchimp.lists.batchListMembers(listID, newUser);
+    console.log(response);
+    // direct to success page
+    res.sendFile(__dirname + "/success.html");
+    console.log(
+      `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+    );
+  };
+
+  run().catch((e) => res.sendFile(__dirname + "/failure.html")); //direct to failure page if error occured
 });
 
-//set port
+//Failure page - Post
+app.post("/failure", function (req, res) {
+  //redirect back to landing page
+  res.redirect("/");
+});
+
+//set up port
 const port = 3000;
 app.listen(port, function () {
-  console.log("Server is runing on port " + port);
+  console.log(`Server is runing on port ${port}.`);
 });
