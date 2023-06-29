a2c62102101c7879513da36db9688009dbfcf5d7
khalilpyh <khalilpyh@gmail.com>
2023-06-29 02:04:50 -0400

    setup landing page and server

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true })); //use body-parser
 app.use(express.static("public")); //use local css and images
+
+//Landing page -  Get
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+//Landing page - Post
+app.post("/", function (req, res) {
+  //gather user input
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+});
+
+//set port
+const port = 3000;
+app.listen(port, function () {
+  console.log("Server is runing on port " + port);
+});
